f034fb492d6f4ce103e8177a0d1317188f3f8aa9
Alphxnse <58533701+alphxnse1301@users.noreply.github.com>
2023-02-23 01:50:24 -0800

    Revert "Merge branch 'master' of https://github.com/CJ5518/Exodus"
    This reverts commit 4a071107704f35d1e6bc65e2e9c5bd32f8752220, reversing
    changes made to 8e44b9da6309d4215c270447bb111c0fff387f81.

--- Assets/Source/Victor/Scripts/EntitySpawner.cs
+++ Assets/Source/Victor/Scripts/EntitySpawner.cs
@@ -17,21 +17,22 @@ public class EntitySpawner : MonoBehaviour
     private GameObject entity;
 
     [SerializeField]
-    private float spawnInterval;
-    
-    
     private Vector3 spawnPosition;
+
+    [SerializeField]
+    private float spawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
+        spawnPosition = transform.position;
+
         InvokeRepeating("SpawnEntity", 1.0f, spawnInterval);
     }
 
     // Update is called once per frame
     private void SpawnEntity()
     {
-        spawnPosition = transform.position;
         Vector3 randomOffset = Random.insideUnitSphere;
         Vector3 randomPosition = spawnPosition + randomOffset;
         randomPosition.z = spawnPosition.z;
