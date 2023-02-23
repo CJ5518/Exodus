4b82d4d6473c23d8d30dbcca355644cf32eda723
unknown <26noahr@pm.me>
2023-02-23 07:49:21 -0800

    update game manager

--- Assets/Source/Noah/GameManager.cs
+++ Assets/Source/Noah/GameManager.cs
@@ -6,12 +6,19 @@ public class GameManager : MonoBehaviour
 {
     private EventManager em;
 
+    private GameObject enemy;
+    private Vector3 up;
+
     // Start is called before the first frame update
     void Start()
     {
         em = Resources.Load<EventManager>("prefabs/Noah/myEventManager");
         Instantiate(em);
  
+        up = new Vector3(0,10,0);
+        enemy = GameObject.Find("BaseEnemy");
+        InvokeRepeating("SpawnEntity", 10.0f, 20);
+
     }
 
     // Update is called once per frame
@@ -19,6 +26,16 @@ public class GameManager : MonoBehaviour
     {
         if(Input.GetKey("h"))
             em.startEvent(1);
+    }
+
+
+    private void SpawnEntity()
+    {   
+        Vector3 randomOffset = Random.insideUnitSphere; 
+        Vector3 randomPosition =up + randomOffset;
+        randomPosition.z = 0;
+
+        Instantiate(enemy, randomPosition, Quaternion.identity);
     }
 }
  
