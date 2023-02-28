9a644a30998e2a250059135d36596d2887e67967
Kalagious <44515549+Kalagious@users.noreply.github.com>
2023-02-27 16:33:28 -0800

    Finished the Stress Test

--- Assets/Test/Jordan/StressTest1/GameManagerStressTest1.cs
+++ Assets/Test/Jordan/StressTest1/GameManagerStressTest1.cs
@@ -8,19 +8,38 @@ public class GameManagerStressTest1 : MonoBehaviour
     public GameObject goBasicEnemy;
     public int iEnemyCount;
     public TextMeshProUGUI tmpEnemyCount;
+    public TextMeshProUGUI tmpFPS;
+    private int iBelow60;
+
     void Start()
     {
         iEnemyCount = 0;
+        iBelow60 = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.frameCount % 10 == 0)
+        float iFramerate = 1 / Time.smoothDeltaTime;
+
+        if (Time.frameCount % 10 == 0 && iBelow60 < 5)
         {
-            Instantiate(goBasicEnemy, new Vector3(0, 0, 0), Quaternion.identity);
-            iEnemyCount++;
-            tmpEnemyCount.SetText("Enemies: " + iEnemyCount);
+            if (iFramerate > 60)
+            {
+                Instantiate(goBasicEnemy, new Vector3(0, 0, 0), Quaternion.identity);
+                iEnemyCount++;
+                tmpEnemyCount.SetText("Enemies: " + iEnemyCount);
+            }
+            else
+            {
+                iBelow60++;
+            }
+
+        }
+
+        if (Time.frameCount % 20 == 0)
+        {
+            tmpFPS.SetText("FPS: " + iFramerate);
         }
     }
 }
