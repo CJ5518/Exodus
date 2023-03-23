bfbabeb5fa9e879c9352c5f362f7b99b51b5299d
unknown <26noahr@pm.me>
2023-03-23 01:45:11 -0700

    finished frog stress test

--- Assets/Source/Noah/FrogScript.cs
+++ Assets/Source/Noah/FrogScript.cs
@@ -9,28 +9,37 @@ public class FrogScript : MonoBehaviour
     private Rigidbody2D rb;
     private int direction;
     private float jumpTimer;
+    private int speedX;
    
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
-        jumpTimer = 0;
+        //speedX = 5; //this is a good default jump speed in x direction
+        jumpTimer = 4;
+    }
+
+    public void receiveSpeed(int speed){
+        speedX = speed;
+        //Debug.Log("received: "+speedX);
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {//Debug.Log("speedX: "+speedX);
          if(player.GetComponent<Rigidbody2D>().transform.position.x < transform.position.x) {
              GetComponent<SpriteRenderer>().flipX = false;
-             direction = -5;
+             direction = 0 - speedX;
          }
          else {
              GetComponent<SpriteRenderer>().flipX = true;
-             direction = 5;
+             direction = speedX;
          }
+         //Debug.Log("speedX: "+speedX);
          jumpTimer += Time.deltaTime;
          if (jumpTimer >= 5.0f){
+             //Debug.Log("direction: "+direction);
              rb.velocity = new Vector2(direction, 5);
              jumpTimer = 0;
          }
--- Assets/Source/Noah/StressTest/StressGameManager.cs
+++ Assets/Source/Noah/StressTest/StressGameManager.cs
@@ -4,69 +4,40 @@ using UnityEngine;
 
 public class StressGameManager : MonoBehaviour
 {
-    private EventManager em;
-
-    private GameObject enemy;
+    private GameObject frog;
+private static GameObject f;
+private GameObject f1;
     private Vector3 up;
-    private int count;
+    private int maxSpeed;
+    private float timer;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        count = 0;
-        em = Resources.Load<EventManager>("prefabs/Noah/myEventManager");
-        Instantiate(em);
- 
-        Debug.Log("press numbers 1-6 to run different boundary and stress tests");
-        //up = new Vector3(0,10,0);
-        //enemy = GameObject.Find("BaseEnemy");
-        //InvokeRepeating("SpawnEntity", 1.0f, 2);
-
+        maxSpeed = 5;
+        timer = 0;
+        frog = Resources.Load<GameObject>("prefabs/Noah/Frog");
     }
 
     // Update is called once per frame
     void Update()
     {
-        count++;
-        if(Input.GetKey("0")){
-            Debug.Log("count of event managers: " + count);
-            EventManager myem = Instantiate(em);
-            myem.startEvent(1, 10, 10);
-
-            if (count > 80) {
-                Time.timeScale = 0;
-                Debug.Log("too many to see");
-                Destroy(this);
-            }
+        //f1;
+        timer += Time.deltaTime;
+        if(timer >= 2){
+            f = Instantiate(frog, new Vector2(4,0), Quaternion.identity, transform);
+            f1 = f;
+            Destroy(f, 2.0f);
+            f.GetComponent<FrogScript>().receiveSpeed(maxSpeed);
+            Debug.Log("stress speed: "+maxSpeed);
+            maxSpeed += 5;
+            timer = 0;
         }
-        /*
-        if(Input.GetKey("0")){
-            Debug.Log("normal run, difficulty=5, time=10 seconds");
-            em.startEvent(1, 5, 10);
+        if(f != null && f.transform.position.x < 0) {
+            Debug.Log("frog clips through wall at jump speed: "+maxSpeed);
+            Destroy(this);
         }
-        if(Input.GetKey("1")){
-            Debug.Log("difficulty boundary test, max difficulty=300, time=10 seconds");
-            em.startEvent(1, 300, 10);
-        }
-        if(Input.GetKey("2")){
-            Debug.Log("difficulty boundary test, difficulty=301, time=10 seconds");
-            em.startEvent(1, 301, 10);
-        }
-        if(Input.GetKey("3")){
-            Debug.Log("event type boundary test, calling event type 2 which doesn't exist (yet)");
-            em.startEvent(2, 5, 5);
-        }
-        */
-    }
-
-
-    private void SpawnEntity()
-    {   
-        Vector3 randomOffset = Random.insideUnitSphere; 
-        Vector3 randomPosition =up + randomOffset;
-        randomPosition.z = 0;
-
-        Instantiate(enemy, randomPosition, Quaternion.identity);
     }
 }
  
