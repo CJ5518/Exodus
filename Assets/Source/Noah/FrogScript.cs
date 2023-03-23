d639dd4b00119adfef92243cff6ed7ac89f23870
unknown <26noahr@pm.me>
2023-03-22 23:16:17 -0700

    frogs jump

--- Assets/Source/Noah/FrogScript.cs
+++ Assets/Source/Noah/FrogScript.cs
@@ -8,33 +8,32 @@ public class FrogScript : MonoBehaviour
     private GameObject player;
     private Rigidbody2D rb;
     private int direction;
+    private float jumpTimer;
    
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindWithTag("Player");
         rb = GetComponent<Rigidbody2D>();
-        //flip = GetComponent<SpriteRenderer>().flipX;
-        FrogBehavior();
+        jumpTimer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-         if(player.GetComponent<Rigidbody2D>().transform.position.x < transform.position.x) 
+         if(player.GetComponent<Rigidbody2D>().transform.position.x < transform.position.x) {
              GetComponent<SpriteRenderer>().flipX = false;
-         else
+             direction = -5;
+         }
+         else {
              GetComponent<SpriteRenderer>().flipX = true;
+             direction = 5;
+         }
+         jumpTimer += Time.deltaTime;
+         if (jumpTimer >= 5.0f){
+             rb.velocity = new Vector2(direction, 5);
+             jumpTimer = 0;
+         }
     }
-   
-    IEnumerator FrogBehavior(){
-       while(true){
-           yield return new WaitForSeconds(5);
-           if(GetComponent<SpriteRenderer>().flipX == false)
-               direction = -5;
-           else
-               direction = 5;
-           rb.velocity = new Vector2(direction, 5);
-       }
-    }
+  
 }
