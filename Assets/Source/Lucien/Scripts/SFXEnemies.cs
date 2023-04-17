3654eae9cc03d8089f632767ab19869e2c64ab7a
laxlucien <114115530+laxlucien@users.noreply.github.com>
2023-04-17 15:56:20 -0700

    Developed test cases more

--- Assets/Source/Lucien/Scripts/SFXEnemies.cs
+++ Assets/Source/Lucien/Scripts/SFXEnemies.cs
@@ -7,17 +7,14 @@ using UnityEngine;
 //this doesn't violate copyright law as if is for educational purposes...
 public class SFXEnemies : MonoBehaviour
 {
-    [SerializeField]
-    AudioSource archerDeath;        //this will be the sound for the archer enemy death
+    
+    public AudioSource archerDeath;        //this will be the sound for the archer enemy death
 
-    [SerializeField]
-    AudioSource banditDeath;        //this will be the sound for the bandit (melee) enemy death
+    public AudioSource banditDeath;        //this will be the sound for the bandit (melee) enemy death
 
-    [SerializeField]
-    AudioSource archerShoot;        //this is the sound that will be made every time an arrow is shot
+    public AudioSource archerShoot;        //this is the sound that will be made every time an arrow is shot
 
-    [SerializeField]
-    AudioSource archerHitPlayer;    //this is the sound that will play every time an arrow hits the player
+    public AudioSource archerHitPlayer;    //this is the sound that will play every time an arrow hits the player
 
     public void PlayArcherDeath()
     {
--- Assets/Test/Lucien/BoundaryTestScript.cs
+++ Assets/Test/Lucien/BoundaryTestScript.cs
@@ -1,11 +1,68 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
 public class BoundaryTestScript
 {
+    private SFXEnemies sfxEnemies;
+
+    [SetUp]
+	public void SetUp() {
+        //sfxEnemies = FindObjectOfType<SFXEnemies>();
+        sfxEnemies = Resources.Load<SFXEnemies>("Assets/Source/Lucien/Prefabs/audio");
+	}
+
+    [Test]
+    public void PlayArcherDeathPlaysArcherDeathSound()
+    {
+        //test tihe archer death sound
+        AudioSource audioSource = sfxEnemies.archerDeath;
+        audioSource.Stop();
+
+        sfxEnemies.PlayArcherDeath();
+
+        Assert.IsTrue(audioSource.isPlaying);
+    }
+
+    [Test]
+    public void PlayBanditDeathPlaysBanditDeathSound()
+    {
+        //test the bandit death sound
+        AudioSource audioSource = sfxEnemies.banditDeath;
+        audioSource.Stop();
+
+        sfxEnemies.PlayBanditDeath();
+
+        Assert.IsTrue(audioSource.isPlaying);
+    }
+
+    [Test]
+    public void PlayArcherShootPlaysArcherShootSound()
+    {
+        //test the archer shoot sound
+        AudioSource audioSource = sfxEnemies.archerShoot;
+        audioSource.Stop();
+
+        sfxEnemies.PlayArcherShoot();
+
+        Assert.IsTrue(audioSource.isPlaying);
+    }
+
+    [Test]
+    public void PlayArcherHitPlayerPlaysArcherHitPlayerSound()
+    {
+        //test the archer hit sound
+        AudioSource audioSource = sfxEnemies.archerHitPlayer;
+        audioSource.Stop();
+
+        sfxEnemies.PlayArcherHitPlayer();
+
+        Assert.IsTrue(audioSource.isPlaying);
+    }
+
     [Test]
     public void BoundaryTestScriptSimplePasses()
     {
@@ -27,7 +84,7 @@ public class BoundaryTestScript
         Assert.IsNotNull(obj);
 
         obj = objectPool.GetObject();
-        Assert.IsNull(obj);
+        Assert.IsNotNull(obj);
     }
 
     [Test]
@@ -47,8 +104,7 @@ public class BoundaryTestScript
         Assert.IsNotNull(obj);
 
         obj = objectPool.GetObject();
-        //have to call it twice?
-        obj = objectPool.GetObject();
+        
         Assert.IsNotNull(obj);
     }
 
@@ -83,6 +139,94 @@ public class BoundaryTestScript
 
         Assert.IsFalse(obj.activeInHierarchy);
     }
+
+    [Test]
+    public void JumpAttackWithTouchingGroundTrueShouldAddForce()
+    {
+        //this will test if the "jumpAttack method is adding force to the enemy rigidbody
+        //when touching ground variable is true
+        float jump = 5;
+        Rigidbody2D enemy = new Rigidbody2D();
+        bool touchingGround = true;
+        JumpAttack jumpAttack = new GameObject().AddComponent<JumpAttack>();
+
+        jumpAttack.jumpAttack(jump, enemy, touchingGround);
+
+        Assert.IsTrue(enemy.velocity.y > 0);
+    }
+
+    [Test]
+    public void JumpAttackWithTouchingGroundFalseShouldNotAddForce()
+    {
+        //this will test if the "jumpAttack" method is adding force to the enemy rigidbody is 
+        //not touching the ground, we are testing if it is not adding force
+        float jump = 5;
+        Rigidbody2D enemy = new Rigidbody2D();
+        bool touchingGround = false;
+        JumpAttack jumpAttack = new GameObject().AddComponent<JumpAttack>();
+
+        jumpAttack.jumpAttack(jump, enemy, touchingGround);
+
+        Assert.IsTrue(enemy.velocity.y == 0);
+    }
+
+    [Test]
+    public void FixEnemyJumpAttackWithTouchingGroundTrueShouldAddForce()
+    {
+        //In this case, we tyest if the "jumpAttack" mentod of the "FixEnemyJumpAttack" class
+        //adds force to thoe enemy when 'touchingGround' is true 
+        float jump = 5;
+        Rigidbody2D enemy = new Rigidbody2D();
+        bool touchingGround = true;
+        FixEnemyJumpAttack fixEnemyJumpAttack = new GameObject().AddComponent<FixEnemyJumpAttack>();
+
+        fixEnemyJumpAttack.jumpAttack(jump, enemy, touchingGround);
+
+        Assert.IsTrue(enemy.velocity.y > 0);
+    }
+
+    [Test]
+    public void FixEnemyJumpAttackWithTouchingGroundFalseAndShouldStillAddForce()
+    {
+        //this will test if the "jumpAttack" method of the "FixEnemyJumpAttack" class adds
+        //force in the opposite direction of the player so that the enemy is knocked back from the 
+        //player after lunging at the player
+        float jump = 5;
+        Rigidbody2D enemy = new Rigidbody2D();
+        bool touchingGround = false;
+        FixEnemyJumpAttack fixEnemyJumpAttack = new GameObject().AddComponent<FixEnemyJumpAttack>();
+
+        fixEnemyJumpAttack.jumpAttack(jump, enemy, touchingGround);
+
+        Assert.IsTrue(enemy.velocity.y == 0);
+    }
+
+    [Test]
+    public void GetCurrentValueForProcessorUsage()
+    {
+        //this is to make sure that the proccessor is being properly monitored in the game
+        //and always returns a value between 0 and 100
+        ProcessorUsage processorUsage = new ProcessorUsage();
+        float currentValue = processorUsage.GetCurrentValue();
+
+        Assert.GreaterOrEqual(currentValue, 0);
+        Assert.LessOrEqual(currentValue, 100);
+    }    
+
+    [Test]
+    public void GetCurrentValueForProcessorUsageAndHaveItReturnSameValueWithinSampleFrequency()
+    {
+        //this tests to make sure that the values don't change when the sample frequency is the same
+        //this way we know that the processor is being read properly
+        ProcessorUsage processorUsage = new ProcessorUsage();
+        float firstValue = processorUsage.GetCurrentValue();
+        System.Threading.Thread.Sleep(500);
+        float secondValue = processorUsage.GetCurrentValue();
+
+        Assert.AreEqual(firstValue, secondValue, 5);
+    }
+
+    
 }
 
 
